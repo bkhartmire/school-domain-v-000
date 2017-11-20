0dbaf2a2ab453d7f2fd75559c06b00ba794be936
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-20 19:50:40 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,4 +5,21 @@ class School
   end
 
   attr_reader :name
+  
+  ROSTER = {}
+  
+  def roster
+    ROSTER
+  end
+  
+  def add_student(name, grade)
+    ROSTER[grade] = []
+    ROSTER[grade] << name
+  end
+
+  def grade
+  end
+  
+  def sort
+  end
 end
