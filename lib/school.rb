bbd97ed9709e04667743a4de2d41d048be743599
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-20 20:10:11 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,12 @@ class School
   attr_accessor :roster, :grade, :sort, :name
 
   def add_student(name, grade)
-    roster[grade] = []
-    roster[grade] << name
+    if roster[grade] == nil
+      roster[grade] = []
+      roster[grade] << name
+    else
+      roster[grade] << name
+    end
     roster
   end
 
